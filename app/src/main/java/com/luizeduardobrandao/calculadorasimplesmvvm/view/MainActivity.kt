2571e4f19496d4f5d1f16405acfadffa242f1450
EduardoBran <eduardo.ads1814@gmail.com>
2025-06-19 15:13:53 -0300

    exibindo o layout na tela

--- app/src/main/java/com/luizeduardobrandao/calculadorasimplesmvvm/view/MainActivity.kt
+++ app/src/main/java/com/luizeduardobrandao/calculadorasimplesmvvm/view/MainActivity.kt
@@ -6,8 +6,12 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.luizeduardobrandao.calculadorasimplesmvvm.R
+import com.luizeduardobrandao.calculadorasimplesmvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +21,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
 
